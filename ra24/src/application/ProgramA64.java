08a6d4e29270bc505fcb528dd508b84178e676e4
Lev_TheDev <levinosecretti@outlook.com>
2024-10-09 15:27:05 -0400

    Criação de Classes e Pacotes distintos.

--- ra24/src/application/ProgramA64.java
+++ ra24/src/application/ProgramA64.java
@@ -3,24 +3,31 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Triangle;
+
 public class ProgramA64 {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		Triangle x, y;
+		x = new Triangle();
+		y = new Triangle();
+		
+		
 		System.out.println("Enter the measure of triangle X:");
-		double xa = sc.nextDouble();
-		double xb = sc.nextDouble();
-		double xc = sc.nextDouble();
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
 		
 		System.out.println("Enter the measure of triangle Y:");
-		double ya = sc.nextDouble();
-		double yb = sc.nextDouble();
-		double yc = sc.nextDouble();
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
 		
-		double ax = xarea(xa, xb, xc);
-		double ay = yarea(ya, yb, yc);
+		double ax = xarea(x.a, x.b, x.c);
+		double ay = yarea(y.a, y.b, y.c);
 		
 		System.out.printf("Triangle X area: %.4f%n", ax);
 		System.out.printf("Triangle Y area: %.4f%n", ay);
